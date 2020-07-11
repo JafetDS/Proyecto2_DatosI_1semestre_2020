7165ad60df37d0316caef61e76a5ee43b66aae62
Jafet Dixon Solano <jafet.dixon@gmail.com>
2020-07-11 07:15:30 -0600

    InicioApp

---
+++ P2XChef/P2XChef/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace P2XChef
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new P2XChef.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
