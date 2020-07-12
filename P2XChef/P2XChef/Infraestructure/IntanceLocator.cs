ed476e076815120d436b36ad48ff2ba7b118d5f2
Jafet Dixon Solano <jafet.dixon@gmail.com>
2020-07-12 01:58:44 -0600

    !!GitHub_Desktop<master>
    Insfaestructura

---
+++ P2XChef/P2XChef/Infraestructure/IntanceLocator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using P2XChef.ViewModels;
+
+namespace P2XChef.Infraestructure
+{
+    public partial class IntanceLocator : Component
+    {
+        #region Atributes
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+            
+
+        #endregion
+        public IntanceLocator()
+        {
+            InitializeComponent();
+        }
+       
+        public IntanceLocator(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ P2XChef/P2XChef/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+
+namespace P2XChef.ViewModels
+{
+    public partial class MainViewModel : Component
+    {
+        public MainViewModel()
+        {
+            InitializeComponent();
+        }
+
+        public MainViewModel(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
