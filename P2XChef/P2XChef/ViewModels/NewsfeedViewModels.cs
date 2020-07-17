ff57ec91a421501866c0ae978959b6524b210fe4
Jafet Dixon Solano <jafet.dixon@gmail.com>
2020-07-16 18:22:42 -0600

    NewFeed

---
+++ P2XChef/P2XChef/ViewModels/NewsfeedViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2XChef.ViewModels
+{
+    public class NewsfeedViewModels
+    {
+        public NewsfeedViewModels()
+        {
+
+        }
+    }
+}
